dd667d56d7f3b1d0313f2847f7d2cc2d7de139f7
배정만 <jmbae@tmon.co.kr>
2017-01-22 14:07:46 +0900

    initial project

---
+++ src/main/java/com/jtdeveloper/SharideApplication.java
@@ -0,0 +1,12 @@
+package com.jtdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharideApplication.class, args);
+	}
+}
